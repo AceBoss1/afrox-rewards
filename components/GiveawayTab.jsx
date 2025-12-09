588e1189b7fa717d12297c274f030f8577b9aee5
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-12-09 11:09:33 +0100

    Update GiveawayTab.jsx

--- components/GiveawayTab.jsx
+++ components/GiveawayTab.jsx
@@ -1,7 +1,3 @@
-// ============================================
-// components/GiveawayTab.jsx
-// ============================================
-
 'use client';
 
 import { useState, useEffect } from 'react';
